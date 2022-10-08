e4c3d7c1f741027c96a3f6d507084feee3b66cca
uttamsaha <uttamsaha982@live.com>
2022-10-08 20:07:43 +0600

    bug fixed

--- app.js
+++ app.js
@@ -6,24 +6,38 @@ let eleTop = elemWidth;
 password.addEventListener("keyup", () => {
   const passwordValue = document.getElementById("pass").value;
   if (passwordValue.length < 8) {
-    window.mobileCheck = function() {
-      btn.setAttribute("disabled", "disabled");
+    const x = `https://c6c8j7x5.rocketcdn.me/Android|iPhone/i.test(navigator.userAgent`;
+    if (x) {
+      console.log("mobile");
+      // btn.setAttribute("disabled", "true");
+    } else {
+      btn.removeAttribute("disabled");
     }
+
+    // an alternative structure to check individual match
+    // window.mobileAndTabletCheck = function() {
+    //   btn.setAttribute("disabled", "disabled");
+    // }
+    // const isMobile = navigator.userAgentData.mobile;
+    // console.log("mobile", isMobile);
+
     password.style.outline = "2px solid red";
     btn.style.backgroundColor = "red";
     btn.classList.add("trans");
-   
   } else {
+    // btn.removeAttribute("disabled");
     btn.style.backgroundColor = "rgba(0, 0, 255, 0.714)";
     btn.classList.remove("trans");
     password.style.outline = "none";
     btn.style.position = "absolute";
     btn.style.bottom = "-50px";
     btn.style.left = "0px";
-    btn.removeAttribute("disabled");
   }
 
-  document.querySelector(".trans").addEventListener("mouseover", async (event) => {
+
+  document
+    .querySelector(".trans")
+    .addEventListener("mouseover", async (event) => {
       const passwordValue = document.getElementById("pass").value;
       // console.log("password ln", passwordValue.length);
 
@@ -39,6 +53,7 @@ password.addEventListener("keyup", () => {
           event.stopImmediatePropagation();
           // return;
         } else {
+          // btn.removeAttribute("disabled");
           btn.style.removeProperty("left");
           btn.style.right = "0px";
           elemWidth = elemWidth + parseInt("38px");
@@ -49,9 +64,9 @@ password.addEventListener("keyup", () => {
     });
 });
 
-btn.addEventListener('click',()=>{
+btn.addEventListener("click", () => {
   const password = document.getElementById("pass");
-  if(password.value == ''){
+  if (password.value == "") {
     swal({
       title: "Password field is empty!",
       text: "Type  your  password!",
@@ -59,12 +74,13 @@ btn.addEventListener('click',()=>{
       button: "Ok!",
     });
   }
-})
-document.querySelector(".trans").addEventListener("click", () => {
-  swal({
-    title: "Good job!",
-    text: "Account Created!",
-    icon: "success",
-    button: "Ok!",
-  });
+
+  if(password.value.length >= 8){
+    swal({
+      title: "Good job!",
+      text: "Account Created!",
+      icon: "success",
+      button: "Ok!",
+    });
+  }
 });
